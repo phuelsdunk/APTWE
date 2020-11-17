94e501ee96313aea872b51332a97bb747af979eb
Hülsdunk, Philipp <mail@huelsdunk.tech>
2020-11-17 17:30:25 +0100

    Initial code upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='APTWE',
+    packages=['aptwe', 'aptwe.addons'],
+    version='0.1',
+    license='MIT',
+    description='Parser framework',
+    author='Philipp Hülsdunk',
+    author_email='mail@huelsdunk.tech')
